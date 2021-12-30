c967952366858f21b6c515e671d2f68660cb9db8
Alkxandr <buitronyealx@gmail.com>
2021-12-29 22:54:51 -0500

    update

---
+++ js/calculadora.js
@@ -0,0 +1,52 @@
+window.addEventListener('load', ()=> { /*Escuchamos cuando se carga el documento*/
+    /* creamos dos constantes y nos guardamos los elementos que necesitamos*/
+    const display = document.querySelector('.calculator-display')
+    const keypadButtons = document.getElementsByClassName('keypad-button')
+
+
+    /* creamos otra constante para convertir el HTMLCollection a Array*/
+    const keypadButtonsArray = Array.from(keypadButtons);
+
+
+    /* iteramos por nuetro nuevo Array de botones*/
+    keypadButtonsArray.forEach ( (button) => {
+
+        /* a cada boton le agregamos un listener*/
+        button.addEventListener('click', () => {
+            calculadora(button, display);
+        })
+    })
+});
+
+function calculadora(button, display) {
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display)
+            break;
+
+        case '=':
+            calcular(display)   
+            break;
+        default:
+            actualizar(display, button);
+            break;
+
+
+    }
+}
+
+function calcular (display) {
+    display.innerHTML = eval(display.innerHTML) /* tomar el string, resolverlo y guardarlo en el innerHTML del display */
+}
+
+function actualizar (display, button) {
+    if (display.innerHTML == 0) {
+         display.innerHTML = '';
+    }
+    display.innerHTML += button.innerHTML;
+    /*display.innerHTML = display.innerHTML + button.innerHTML*/
+}
+
+function borrar(display) {
+    display.innerHTML = 0;
+}
